6de28674c13cb1e067b55f38d4c3279339064545
Shahmir Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2023-05-27 05:01:33 -0700

    Update settings.py

--- ShareSwift/settings.py
+++ ShareSwift/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'ShareSwift.wsgi.application'
    # }
 #}
 DATABASES = {
-    'default': dj_database_url.parse('postgres://memeswift_database_user:gdY0kivR76zUzU4BOE4vYpnrtil7fJ1z@dpg-chl5j4u4dadfmsh99etg-a.ohio-postgres.render.com/memeswift_database'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
