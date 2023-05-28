b8c5bebaa4b9e38494e2e6891c3e94567a3fa40b
Shahmir Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2023-05-28 10:23:05 -0700

    Update views.py

--- base/views.py
+++ base/views.py
@@ -23,35 +23,23 @@ def meme(request):
         meme_obj = Meme(raw_photo=image)
         meme_obj.save()
 
-<<<<<<< HEAD
         picture_url = f"https://{request.get_host()}/{meme_obj.raw_photo}"
-=======
-        picture_url = f"https://memeswiftai.pythonanywhere.com/media/342920087_1281416352800985_6484091355838271538_n.jpg"
->>>>>>> 865f0bcf0be0bcab9bd96bf32927c9dfe1479d0f
         caption = get_caption(picture_url)
 
         meme_text = generate_meme_text(caption, context)
 
 
-<<<<<<< HEAD
         meme = generate_meme(image, meme_text)
-=======
-        meme = generate_meme(image, caption)
->>>>>>> 865f0bcf0be0bcab9bd96bf32927c9dfe1479d0f
         meme = watermark(meme)
 
         meme_obj = Meme(photo=meme, caption=caption, meme_text=meme_text)
         meme_obj.save()
-<<<<<<< HEAD
 
         return render(request, 'result.html', {'meme_text': meme_obj.meme_text, 'caption': caption, 'meme': meme_obj.photo, 'url': picture_url})
 
 
-=======
->>>>>>> 865f0bcf0be0bcab9bd96bf32927c9dfe1479d0f
 
-        return render(request, 'result.html', {'meme_text': meme_obj.meme_text, 'caption': caption, 'meme': meme_obj.photo, 'url': picture_url})
-
+       
 
 
     return render(request, 'result.html')
