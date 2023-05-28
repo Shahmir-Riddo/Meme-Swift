1f57c6380642a0bb692ed1f10e0290235af4e6aa
Shahmir Riddo <78295593+Shahmir-Riddo@users.noreply.github.com>
2023-05-28 11:05:07 -0700

    Update settings.py

--- ShareSwift/settings.py
+++ ShareSwift/settings.py
@@ -106,11 +106,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+MEDIA_ROOT= os.path.join(BASE_DIR, "media")
+
 MEDIA_URL = '/media/'
 
 # Default primary key field type
